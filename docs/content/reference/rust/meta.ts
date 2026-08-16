3d0dba8509279db7fc9c2254d44340d40052e888
Andrew Nijmeh <andrewnijmeh1@gmail.com>
2026-08-16 13:50:19 -0400

    docs: rust sdk reference (#201)

---
+++ docs/content/reference/rust/meta.ts
@@ -0,0 +1,16 @@
+import { defineMeta } from "blume";
+
+export default defineMeta({
+  title: "Rust SDK",
+  icon: "braces",
+  pages: [
+    "index",
+    "quickstart",
+    "environments",
+    "hosts",
+    "snapshots",
+    "api-keys",
+    "errors",
+    "advanced",
+  ],
+});
